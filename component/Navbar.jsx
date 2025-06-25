3f7e02f00903060515096a393c6763af8dffa924
BETHEL <bethelndegonekwu@gmail.com>
2025-06-25 23:53:57 +0100

    Update Changes

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,37 +1,31 @@
 "use client"
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
     const [ isOpen, setIsOpen ] = useState(false);
+    const pathname = usePathname()
 
     const handleToggle = () => {
-        if (!isOpen) {
-            document.body.style.overflow = 'hidden'
-        } else {
-            document.body.style.overflow = 'auto'
-        }
-        setIsOpen(!isOpen)
+        setIsOpen(prev => {
+            const newState = !prev;
+            document.body.style.overflow = newState ? 'hidden' : 'auto';
+            return newState;
+        });
     }
 
+    useEffect(() => {
+        setIsOpen(false);
+        document.body.style.overflow = 'auto';
+    }, [pathname]);
+
     const categories = [
-        {
-            name: 'Fashion',
-            link: 'fashion',
-            delay: 0
-        },
-        {
-            name:  'Lifestyle',
-            link: 'lifestyle',
-            delay: 0.1
-        },
-        {
-            name:  'Mental Health',
-            link: 'health',
-            delay: 0.2
-        }
+        { name: 'Fashion', link: 'fashion', delay: 0 },
+        { name:  'Lifestyle', link: 'lifestyle', delay: 0.1 },
+        { name:  'Mental Health', link: 'health', delay: 0.2 }
     ]
 
     const variants = {
@@ -68,28 +62,28 @@ const Navbar = () => {
                 className='flex lg:flex-col lg:justify-between lg:items-center lg:h-[55%] gap-x-16'
             >
 
-            {
-                categories.map((cat, index) => (
-                    <Link
-                        href={`/category/${cat.link}`}
-                        key={index} 
-                    >
-                        <motion.li
-                            className='font-semibold cursor-pointer border-b-2 border-transparent hover:border-gren animi'
-                            initial= 'hidden'
-                            animate="visible"
-                            variants={{
-                                hidden: { opacity: 0, y: -150 },
-                                visible: { opacity: 1, y: 0 }
-                            }}
-                            transition={{ duration: 1.8, delay: cat.delay }}
-                            onClick={handleToggle}
+                {
+                    categories.map((cat, index) => (
+                        <Link
+                            href={`/category/${cat.link}`}
+                            key={index} 
                         >
-                            {cat.name}
-                        </motion.li>
-                    </Link>
-                ))
-            }
+                            <motion.li
+                                className='font-semibold cursor-pointer border-b-2 border-transparent hover:border-gren animi'
+                                initial= 'hidden'
+                                animate="visible"
+                                variants={{
+                                    hidden: { opacity: 0, y: -150 },
+                                    visible: { opacity: 1, y: 0 }
+                                }}
+                                transition={{ duration: 1.8, delay: cat.delay }}
+                                onClick={handleToggle}
+                            >
+                                {cat.name}
+                            </motion.li>
+                        </Link>
+                    ))
+                }
 
             </ul>
 
@@ -109,23 +103,23 @@ const Navbar = () => {
                 </motion.button>
             </Link>
                 
-        </div>
+            </div>
 
-        <motion.div 
-            className='animi space-y-1 absolute hidden right-3 bg-gren p-2.5 rounded-full z-40 lg:flex flex-col items-end' 
-            initial= 'hidden'
-            animate="visible"
-            variants={{
-                hidden: { opacity: 0, x: 200},
-                visible: { opacity: 1, x: 0 }
-            }}
-            transition={{ duration: 0.8, delay: 0.5 }}
-            onClick={handleToggle}
-        >
-            <span className={`animi block w-5 h-[2px] bg-black rounded-md ${isOpen ? 'rotate-[45deg] translate-y-2' : ''}`}></span>
-            <span className={`animi block w-4 h-[2px] bg-black rounded-md ${isOpen ? 'opacity-0' : ''}`}></span>
-            <span className={`animi block w-5 h-[2px] bg-black rounded-md ${isOpen ? '-rotate-[45deg] -translate-y-[5px]' : ''}`}></span>
-        </motion.div>
+            <motion.div 
+                className='animi space-y-1 absolute hidden right-3 bg-gren p-2.5 rounded-full z-40 lg:flex flex-col items-end' 
+                initial= 'hidden'
+                animate="visible"
+                variants={{
+                    hidden: { opacity: 0, x: 200},
+                    visible: { opacity: 1, x: 0 }
+                }}
+                transition={{ duration: 0.8, delay: 0.5 }}
+                onClick={handleToggle}
+            >
+                <span className={`animi block w-5 h-[2px] bg-black rounded-md ${isOpen ? 'rotate-[45deg] translate-y-2' : ''}`}></span>
+                <span className={`animi block w-4 h-[2px] bg-black rounded-md ${isOpen ? 'opacity-0' : ''}`}></span>
+                <span className={`animi block w-5 h-[2px] bg-black rounded-md ${isOpen ? '-rotate-[45deg] -translate-y-[5px]' : ''}`}></span>
+            </motion.div>
 
         </motion.div>
     );
